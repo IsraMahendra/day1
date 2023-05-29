8c6919481d647b6946b1fa6ea4e8f71c3f6de9aa
israma <firdhaninisa@gmail.com>
2023-05-29 17:26:50 +0700

    first commit

---
+++ Logic01/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+using System;
+
+OperatorAritmatika();
+
+Console.ReadKey();
+
+static void OperatorAritmatika()
+{
+    int mangga, apel, hasil = 0;
+
+    Console.WriteLine("--Operator aritmatika--");
+    Console.Write("Masukkan mangga : ");
+    mangga = int.Parse(Console.ReadLine());
+    Console.Write("Masukkan apel : ");
+    apel = int.Parse(Console.ReadLine());
+
+    hasil = mangga + apel;
+
+    Console.WriteLine($"hasil mangga + apel = { hasil}");
+}
+
+static void Konversi()
+{
+    Console.WriteLine("--Konversi--");
+
+
+    int myInt = 10;
+    double myDouble = 5.25;
+    bool myBool = true;
+
+    string strMyInt = Convert.ToString(myInt); 
+    string strMyInt2 = myInt.ToString();
+
+    Console.WriteLine(strMyInt);
+    Console.WriteLine(strMyInt2);
+    Console.WriteLine(Convert.ToDouble(myInt));    //Convert int to double
+    Console.WriteLine(Convert.ToInt32(myDouble));  // Convert double to int
+    Console.WriteLine(Convert.ToString(myBool));   //Convert bool to string
+}
---
+++ isra/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace isra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("== BIODATA ==");
+            Console.Write("NAMA\t: ");
+            Console.WriteLine("Isra ");
+            Console.WriteLine("Alamat\t: Bintaro 9");
+
+            Console.WriteLine();
+
+            Console.WriteLine("== PROGRAM KEYEN ==");
+            Console.Write("Tuliskan nama kamu : ");
+            string Nama = Console.ReadLine();
+            Console.WriteLine("Hallow, {0} Selamat Datang", Nama);
+
+            //inplisit
+            var namaweb = "Petani Kode";
+            var umur = 18;
+            var isMenikah = false;
+            Console.WriteLine("nama : " + namaweb);
+            Console.WriteLine("umur : " + umur);
+            Console.WriteLine("IsMenikah : " + isMenikah);
+        }
+    }
+}
